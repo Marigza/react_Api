eb4d2615d305399f2c7d8bca4a0f169093a5f535
mikhaelHan <hancharuk270185@mail.ru>
2023-08-17 13:31:44 +0300

    feat: add invalid adding customers

--- src/client_Api/index.ts
+++ src/client_Api/index.ts
@@ -1,11 +1,11 @@
 import { IgetProject } from './types';
 
 const projectKey: string = 'just-develop23';
-const clientId: string = 'Y9pvMOb17U_r6UZgw88xMLfc';
-const clientSecret: string = 'CTSei9hn6l50tkMSS7ewfHT9fLzLJ_F7';
+const clientId: string = '2PT-eztNLU3wgvgDpf8UwSxZ';
+const clientSecret: string = 'IunouGO33BJdWp3ge2ocqTJBvuSSCJpq';
 const region: string = 'europe-west1.gcp';
 const scope: string[] = [
-  'view_products:just-develop23 manage_my_quotes:just-develop23 manage_my_payments:just-develop23 view_published_products:just-develop23 view_categories:just-develop23 create_anonymous_token:just-develop23 manage_my_business_units:just-develop23 manage_my_shopping_lists:just-develop23 manage_my_quote_requests:just-develop23 manage_my_profile:just-develop23 manage_my_orders:just-develop23',
+  'manage_project:just-develop23 view_audit_log:just-develop23 manage_api_clients:just-develop23',
 ];
 
 export const getToken = async () => {
@@ -26,21 +26,33 @@ export const getToken = async () => {
   return responseData;
 };
 
-export const getProject = async (obj: IgetProject) => {
+export const setInProject = async (objToken: IgetProject, endPath: string, body: string) => {
   const baseUrl = `https://api.${region}.commercetools.com/${projectKey}/`;
-  // const requestBody = `grant_type=client_credentials&scope=${scope.join(' ')}`;
+
+  const response = await fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': `${objToken.token_type} ${objToken.access_token}`,
+    },
+    body: body,
+  });
+
+  return await response.json();
+};
+
+export const getInProject = async (objToken: IgetProject, endPath: string) => {
+  const baseUrl = `https://api.${region}.commercetools.com/${projectKey}/${endPath}`;
 
   const response = await fetch(baseUrl, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': `${obj.token_type} ${obj.access_token}`,
+      'Authorization': `${objToken.token_type} ${objToken.access_token}`,
     },
-    // body: requestBody,
   });
 
-  const responseData = await response.json();
-  console.log(responseData);
+  return await response.json();
 };
 
 /*
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,12 +1,19 @@
 import { FC } from 'react';
 
 import './Login.scss';
-import { getToken, getProject } from '../client_Api';
+import { getToken, getInProject, setInProject } from '../client_Api';
 
 const Login: FC = () => {
   const validation = async () => {
-    const responce = await getToken();
-    getProject(responce);
+    const requestBody = {
+      email: 'hancharuk270185@mail.ru',
+      password: '#Qwerty12345$',
+    };
+    const objToken = await getToken();
+    setInProject(objToken, 'customers', JSON.stringify(requestBody)).then(responce =>
+      console.log(responce)
+    );
+    getInProject(objToken, 'customers').then(responce => console.log(responce));
     return;
   };
 
