8303f1f34a665f961a966b1e92b1c8f49fc880cb
Marigza <marigola@mail.ru>
2023-08-17 22:14:45 +0300

    feat add posting registration, post bill, ship address

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,14 @@
 import { FC } from 'react';
 
 import './Login.scss';
+import { RegObj } from '../bodyObjects/ReggistrationFields';
 import { getToken, getInProject, setInProject } from '../client_Api';
 
 const Login: FC = () => {
   const validation = async () => {
     const requestBody = {
-      email: 'hancharuk345678@mail.ru',
-      password: '#Qwerty888888$',
+      email: 'mamama@mail.ru',
+      password: '#Qwerty8234$',
     };
 
     const objToken = await getToken();
@@ -15,9 +16,42 @@ const Login: FC = () => {
     //   console.log(responce)
     // );
 
-    getInProject(objToken, 'customers/6afb3d83-3f25-4540-a456-6ee7592a9ad1').then(responce =>
-      console.log(responce)
-    );
+    // отправка полей из формы регистрации в commercetools
+    // оставляю закоментированным, т.к. приходит ошибка для одних и тех же данных
+
+    // setInProject(
+    //   objToken,
+    //   'customers/2119541f-de45-47fe-bed1-4c8d09c93b44',
+    //   JSON.stringify(RegObj)
+    // ).then(responce => {
+    //   console.log(responce.addresses);
+    //   return responce.addresses;
+    // });
+
+    getInProject(objToken, 'customers').then(responce => console.log(responce));
+
+    // установка адреса в качестве адреса доставки и юридического адреса
+
+    getInProject(objToken, 'customers/2119541f-de45-47fe-bed1-4c8d09c93b44').then(responce => {
+      console.log(responce);
+      console.log(responce.addresses);
+      setInProject(
+        objToken,
+        'customers/2119541f-de45-47fe-bed1-4c8d09c93b44',
+        JSON.stringify({
+          version: 8, // повторные запросы по тем же кастомерам
+          actions: [{ action: 'addShippingAddressId', addressId: responce.addresses[0].id }],
+        })
+      );
+      setInProject(
+        objToken,
+        'customers/2119541f-de45-47fe-bed1-4c8d09c93b44',
+        JSON.stringify({
+          version: 8, // повторные запросы по тем же кастомерам
+          actions: [{ action: 'addBillingAddressId', addressId: responce.addresses[1].id }],
+        })
+      );
+    });
     return;
   };
 
